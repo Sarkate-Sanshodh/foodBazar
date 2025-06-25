272b7edf73df1bd4aeab9c11776292a5c0959b0e
Sarkate-Sanshodh <150370676+Sarkate-Sanshodh@users.noreply.github.com>
2025-06-25 17:15:30 +0530

    Add files via upload

---
+++ FoodBazar/css/js/script.js
@@ -0,0 +1,9 @@
+//   navbar = document.querySelector('.header .flex .navbar');
+
+// document.querySelector('#menu-btn').onclick = () =>{
+//     navbar.classList.toggle('active');
+// }
+function myfunction(){
+  var nav = document.getElementById('navbar');
+  navbar.classList.toggle('show');
+}
